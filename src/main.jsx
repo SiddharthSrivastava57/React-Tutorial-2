7d9e506e835edec63d7eb52658021bd7f549b99c
SiddharthSrivastava57 <srivastavasid57@gmail.com>
2024-06-19 01:33:50 +0530

    new commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-// src/main.jsx or src/index.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';  // Import the Tailwind CSS file
+import './index.css'; 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
